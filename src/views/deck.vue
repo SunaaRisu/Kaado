8fa9f73081f0d089ae8ac6235238820d4b035ceb
Sunaa <sunaa@sunaarisu.de>
2024-01-03 00:40:35 +0100

    added stack finished animation commented script

--- src/views/deck.vue
+++ src/views/deck.vue
@@ -1,148 +1,183 @@
 <script setup>
     import { ref } from 'vue';
     import { useRoute, useRouter } from 'vue-router'
+    import JSConfetti from 'js-confetti'
+    import { shuffle, arrayWithoutElementAtIndex } from '../../lib/array'
 
     const route = useRoute();
     const router = useRouter();
 
-    const deck = ref();
-
-    const request = {
-        method: 'POST',
-        credentials: 'include',
-        headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
-        body: JSON.stringify({ _id: route.params.id })
+    
+    // setup confetti
+    const confetti = new JSConfetti();
+    function showConfetti() {
+        confetti.addConfetti()
     }
 
-    await fetch('http://localhost:3000/deck/get_one', request)
-        .then(response => {
-            switch(response.status) {
-                case 200:
-                    return response.json();
-                    break;
 
-                case 404:
-                    alert('deck not found');
-                    break;
-                
-                case 500:
-                    alert('something went wrong');
-                    break;
-            
-                default:
-                    alert('something went wrong');
-                    break;
-            };
-        })
-        .then(data => {
-            if(data) {
-                deck.value = data;
-            }                    
-        })
-        .catch(err => {
-            console.log(err);
-            alert('something went wrong');
-        });
+    const deck = ref(); // Holds the original deck fetched by "fetchDeck()".
+    const finished = ref(false); // Is true if all cards has been answerd.
+    const haveFailedCards = ref(false); // Is true if cards were put back into the stack.
+    const stack = ref([]); // Stack created by "createDeckAndStart()".
+    const currentCard = ref(0); // Current card asked.
+    const cardAnswered = ref(false); // Is true if the current card has been answerd.
+    const failedCards = ref([]); // Holds the card that were put back into the stack.
+    
 
-
-    function shuffle(array) {
-        var currentIndex = array.length,  randomIndex;
-
-        // While there remain elements to shuffle.
-        while (currentIndex > 0) {
-
-            // Pick a remaining element.
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex--;
-
-            // And swap it with the current element.
-            [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex], array[currentIndex]];
+    // Fetches the deck with the id from the route and sets "deck" equal to it.
+    async function fetchDeck() {
+        const request = {
+            method: 'POST',
+            credentials: 'include',
+            headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
+            body: JSON.stringify({ _id: route.params.id })
         }
 
-        return array;
-    }
-    
-    const arrayWithoutElementAtIndex = function (arr, index) {
-        return arr.filter(function(value, arrIndex) {
-            return index !== arrIndex;
-        });
-    }
+        await fetch('http://localhost:3000/deck/get_one', request)
+            .then(response => {
+                switch(response.status) {
+                    case 200:
+                        return response.json();
+                        break;
 
-    const cardsData = ref();
-    const cards = ref([]);
-
-    cardsData.value = deck.value.deck.cards;
-
-    for (let index = 0; index < deck.value.deck.chartDefinition.chart_columns; index++) {
-
-        cardsData.value.forEach(element => {
-            cards.value.push({
-                q: element.cardContent[index],
-                a: arrayWithoutElementAtIndex(element.cardContent, index)
+                    case 404:
+                        alert('deck not found');
+                        break;
+                    
+                    case 500:
+                        alert('something went wrong');
+                        break;
+                
+                    default:
+                        alert('something went wrong');
+                        break;
+                };
             })
-        });
+            .then(data => {
+                if(data) {
+                    deck.value = data;
+                }                    
+            })
+            .catch(err => {
+                console.log(err);
+                alert('something went wrong');
+            });
+    }   
+    
+    // Creates a stack from the deck based on the choosen settings, shuffles it and resets "currentCard", "cardAnswerd", "finished" and "failedCards".
+    function createStackAndStart() {
+        const TempCardsData = ref();
+
+        stack.value = [];
+
+        TempCardsData.value = deck.value.deck.cards;
+
+        for (let index = 0; index < deck.value.deck.chartDefinition.chart_columns; index++) {
+
+            TempCardsData.value.forEach(element => {
+                stack.value.push({
+                    q: element.cardContent[index],
+                    a: arrayWithoutElementAtIndex(element.cardContent, index)
+                })
+            });
+        }
+        
+        stack.value = shuffle(stack.value);
+        currentCard.value = 0;
+        cardAnswered.value = false;
+        finished.value = false;
+        failedCards.value = [];
     }
-    
-    cards.value = shuffle(cards.value);
 
-    const currentCard = ref(0);
-    const cardAnswered = ref(false);
-    
-
-    function nextCard() {
+    // Shows the Answer or increases "currentCard" by 1 based on "cardAnswered". If the stack is finished it displays the menu.
+    function showAnswerOrNextCard() {
 
         if (!cardAnswered.value) {
             cardAnswered.value = !cardAnswered.value
         }else{
             cardAnswered.value = !cardAnswered.value
 
-            if (currentCard.value < cards.value.length - 1) {
+            if (currentCard.value < stack.value.length - 1) {
                 currentCard.value++;
             }else{
-                router.push({ path: '/'})
+                finished.value = true;
+                showConfetti();
+                if (failedCards.value.length > 0) {
+                    haveFailedCards.value = true;
+                } else {
+                    haveFailedCards.value = false;
+                }
             }
         }        
     }
 
+    // Pushes current card to "failedCards" and puts the card back into the next 10 cards of the current stack.
     function askCardAgain() {
-        cards.value.splice(currentCard.value + (Math.floor(Math.random() * 10) + 1), 0, cards.value[currentCard.value]);
+        failedCards.value.push(stack.value[currentCard.value]);
 
-        nextCard();
+        stack.value.splice(currentCard.value + (Math.floor(Math.random() * 10) + 1), 0, stack.value[currentCard.value]);
+
+        showAnswerOrNextCard();
     }
 
+    // Pushes current card to "failedCards" and puts the card at the end of the current stack.
     function moveCardBack() {
-        cards.value.push(cards.value[currentCard.value]);
+        failedCards.value.push(stack.value[currentCard.value]);
 
-        nextCard();
+        stack.value.push(stack.value[currentCard.value]);
+
+        showAnswerOrNextCard();
     }
+
+    // Creates a stack from the previously failed cards, shuffles it and resets "currentCard", "cardAnswerd", "finished" and "failedCards".
+    function repeatFailedCards() {
+        currentCard.value = 0;
+        cardAnswered.value = false;
+        finished.value = false;
+        stack.value = failedCards.value;
+        stack.value = shuffle(stack.value);
+        failedCards.value = [];
+    }
+    
+
+    await fetchDeck();
+
+    createStackAndStart();
     
 </script>
 
 <template>
     <main>
-        <span id="title">{{ deck.deck.title }}</span>
+        <span id="title" v-if="!finished">{{ deck.deck.title }}</span>
 
-        <div id="card">
-            <span>{{ cards[currentCard].q }}</span>
+        <div class="card" id="card" v-if="!finished">
+            <span>{{ stack[currentCard].q }}</span>
             <div id="answer" v-if="cardAnswered">
-                <span>{{ cards[currentCard].a[0] }}</span>
-                <span>{{ cards[currentCard].a[1] }}</span>
+                <span>{{ stack[currentCard].a[0] }}</span>
+                <span>{{ stack[currentCard].a[1] }}</span>
             </div>          
         </div>
 
-        <div id="btnContainer" v-if="cardAnswered">
-            <div class="smallBtn" id="againBtn" @click="askCardAgain()"><strong>Again</strong></div>
-            <div class="smallBtn" id="MoveToBackBtn" @click="moveCardBack()"><strong>Move Back</strong></div>
-            <div class="smallBtn" id="nextBtn" @click="nextCard()"><strong>Next</strong></div>
+        <div class="card" id="menu" v-if="finished">
+            <span id="menuTitle">DECK FINISHED</span>
+            <span id="menuSubtitle">{{ deck.deck.title }}</span>
+            <button class="menuBtn" id="repeatDeck" @click="createStackAndStart()"><strong>REPEAT DECK</strong></button>
+            <button class="menuBtn" id="repeatFailedCards" @click="repeatFailedCards()" :disabled="!haveFailedCards"><strong>REPEAT THE {{ failedCards.length }} FAILED CARDS</strong></button>
+            <button class="menuBtn" id="backToSelection" @click="router.push({ path: '/'})"><strong>BACH TO DECK SELECTION</strong></button>
         </div>
-        <div class="btn" id="showAnswerBtn" v-if="!cardAnswered" @click="nextCard()"><strong>Show Answer</strong></div>
 
-        <div id="progressBar">
+        <div id="btnContainer" v-if="cardAnswered && !finished">
+            <div class="smallBtn" id="againBtn" @click="askCardAgain()"><strong>AGAIN</strong></div>
+            <div class="smallBtn" id="MoveToBackBtn" @click="moveCardBack()"><strong>MOVE BACK</strong></div>
+            <div class="smallBtn" id="nextBtn" @click="showAnswerOrNextCard()"><strong>NEXT</strong></div>
+        </div>
+        <div class="btn" id="showAnswerBtn" v-if="!cardAnswered && !finished" @click="showAnswerOrNextCard()"><strong>SHOW ANSWER</strong></div>
+
+        <div id="progressBar" v-if="!finished"> 
             <div id="progressBarText">
-                <span>{{ currentCard + 1 }} / {{ cards.length }}</span>
+                <span>{{ currentCard + 1 }} / {{ stack.length }}</span>
             </div>
-            <div id="progressBarFill" :style="{'width': (currentCard / cards.length) * 100 + '%'}"></div>
+            <div id="progressBarFill" :style="{'width': ((currentCard + 1) / stack.length) * 100 + '%'}"></div>
         </div>
     </main>    
 </template>
@@ -152,12 +187,14 @@
         width: 100vw;
         height: 100vh;
         display: flex;
-        flex-direction: column;
+        flex-direction: row;
         align-items: center;
-        justify-content: space-around;
+        justify-content: center;
     }
 
     #title {
+        position: absolute;
+        top: 10px;
         color: aliceblue;
     }
 
@@ -175,6 +212,9 @@
         align-items: center;
 
         display: flex;
+
+        position: absolute;
+        bottom: 70px;
     }
 
     #btnContainer {
@@ -184,6 +224,9 @@
         display: flex;
         justify-content: space-between;
         align-items: center;
+
+        position: absolute;
+        bottom: 70px;
     }
 
     .smallBtn {
@@ -197,8 +240,7 @@
         align-items: center;        
     }
 
-    #card {
-        margin-top: 100px;
+    .card {
         background-color: aqua;
         height: 500px;
         width: 400px;
@@ -217,6 +259,39 @@
         justify-content: space-between;
         align-items: center;
         flex-direction: column;
+    }
+
+    #menu {
+        display: flex;
+        justify-content: flex-start;
+    }
+
+    #menuTitle {
+        margin-top: 100px;
+        margin-bottom: 5px;
+        font-size: 24px;
+    }
+
+    #menuSubtitle {
+        margin-bottom: 80px;
+    }
+
+    .menuBtn {
+        background-color: rgb(0, 211, 211);
+        border: none;
+
+        width: 80%;
+        height: 50px;
+        border-radius: 5px;
+        margin-top: 30px;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .menuBtn strong{
+        font-size: 16px;
     }
 
     #progressBar {
